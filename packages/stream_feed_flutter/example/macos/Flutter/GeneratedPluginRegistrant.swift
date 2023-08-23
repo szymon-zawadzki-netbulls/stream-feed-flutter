51d6662fa9a97a443d85036bff05e6a690b6261b
Szymon Zawadzki <szymon.zawadzki@netbulls.io>
2023-08-23 08:39:38 +0200

    Upgrade flutter version and photo view plugin

--- packages/stream_feed_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/stream_feed_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import package_info_plus
 import path_provider_foundation
 import url_launcher_macos
-import wakelock_macos
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
